67bb93abd0c1a3e5de0ff71778f33d920adb028f
keanedalisay <keandals21@gmail.com>
2024-10-23 10:12:58 +0800

    Add main calculator interface

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  // manage the sum section
+  int sum = 0;
+
+  // either use a TextEditingController for each input field to get the value
+  TextEditingController add1Controller = TextEditingController();
+
+  // or store each value in the state
+  int firstAddNum = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Unit 5 Calculator"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Add Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(labelText: "First Number"),
+                  // update the firstAddNum state
+                  onChanged: (value) {
+                    setState(() {
+                      firstAddNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+              const Text(" + "),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Second Number"),
+                ),
+              ),
+
+              Text(' = $sum'),
+              // 3.a Add an IconButton here
+
+              // b.b Add an button here
+            ],
+          ),
+
+          // 3.c - Add the other operations
+          // Minus Row
+
+          // Multiplication Row
+
+          // Division Row
+        ],
+      ),
+    );
+  }
+}
